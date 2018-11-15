ecbb20e7e1382c1b49eb97c730641dff3ac94ad0
chrzyki <christoph@functor-argument.net>
2018-11-15 11:42:13 +0100

    Might as well try to fix the Travis error.

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
     zip_safe=False,
     python_requires='>=3.5',
     install_requires=[
+        'attrs>=18.1',
         'pylexibank>=0.7,<0.10',
         'pyconcepticon',
         'pyglottolog',
